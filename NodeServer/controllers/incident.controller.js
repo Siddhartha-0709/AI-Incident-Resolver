dcd48a2a49334856cb2047ec92c16cea70e80726
Siddhartha <siddharthamukherjee0709@gmail.com>
2025-08-04 15:34:32 +0530

    dotenv error

--- NodeServer/controllers/incident.controller.js
+++ NodeServer/controllers/incident.controller.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import incidentModel from "../models/incident.model.js";
 import { GoogleGenAI } from '@google/genai';
+import 'dotenv/config'
 
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
